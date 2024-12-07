a695594c5830fe926eff9bd7c6863b4e35323ac9
luxun <geekluxun@l163.com>
2024-12-07 14:23:52 +0800

    [change] 1.qwen-vl推理示例

---
+++ qwen/qwen_vl.py
@@ -0,0 +1,72 @@
+from qwen_vl_utils import process_vision_info
+from transformers import Qwen2VLForConditionalGeneration, AutoProcessor
+import torch
+
+def generate():
+    # default: Load the model on the available device(s)
+    model = Qwen2VLForConditionalGeneration.from_pretrained(
+        "Qwen/Qwen2-VL-7B-Instruct", torch_dtype="float16", device_map="auto"
+    )
+
+    # We recommend enabling flash_attention_2 for better acceleration and memory saving, especially in multi-image and video scenarios.
+    # model = Qwen2VLForConditionalGeneration.from_pretrained(
+    #     "Qwen/Qwen2-VL-7B-Instruct",
+    #     torch_dtype=torch.bfloat16,
+    #     attn_implementation="flash_attention_2",
+    #     device_map="auto",
+    # )
+
+    # default processer
+    # 需要限制图像大小，否则会在计算自注意力时候内存爆掉，或者使用flash_attention_2
+    min_pixels = 256 * 28 * 28
+    max_pixels = 1280 * 28 * 28
+    processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-7B-Instruct",min_pixels=min_pixels, max_pixels=max_pixels)
+
+    # The default range for the number of visual tokens per image in the model is 4-16384.
+    # You can set min_pixels and max_pixels according to your needs, such as a token range of 256-1280, to balance performance and cost.
+    # min_pixels = 256*28*28
+    # max_pixels = 1280*28*28
+    # processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-7B-Instruct", min_pixels=min_pixels, max_pixels=max_pixels)
+
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image",
+                    "image": "/Users/luxun/Pictures/c801cb49b42c45a8beea30028ba5fe85.jpeg"
+                },
+                {"type": "text", "text": "用中文描述下这张图片."},
+            ],
+        }
+    ]
+
+    # Preparation for inference
+    text = processor.apply_chat_template(
+        messages, tokenize=False, add_generation_prompt=True
+    )
+    image_inputs, video_inputs = process_vision_info(messages)
+    inputs = processor(
+        text=[text],
+        images=image_inputs,
+        videos=video_inputs,
+        padding=True,
+        return_tensors="pt",
+    )
+    inputs = inputs.to("mps")
+
+    # Inference: Generation of the output
+    generated_ids = model.generate(**inputs, max_new_tokens=128)
+    generated_ids_trimmed = [
+        out_ids[len(in_ids):] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+    ]
+    output_text = processor.batch_decode(
+        generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+    )
+    print(output_text)
+
+
+if __name__ == "__main__":
+    import  os
+    os.environ["TOKENIZERS_PARALLELISM"]= "false"
+    generate()
